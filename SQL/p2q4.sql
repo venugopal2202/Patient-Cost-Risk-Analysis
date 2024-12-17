bf7ef4968e7a023c7fdd304a5854e7eb2ce41db8
Venu Gopal <venuchavan2202@gmail.com>
2024-12-17 13:19:43 +0530

    Add files via upload

---
+++ SQL/p2q4.sql
@@ -0,0 +1,33 @@
+/*Assessing Payer Contributions for Different Procedure Types
+Q 0.4
+Objective:
+Analyze payer contributions for the base cost of procedures and identify any gaps between total claim cost and payer coverage.**/
+
+WITH Procedure_Costs AS (
+    SELECT 
+        p.CODE AS ProcedureCode,
+        p.DESCRIPTION AS ProcedureDescription,
+        e.PAYER,
+        SUM(CAST(p.BASE_COST AS DECIMAL(18,2))) AS TotalBaseCost,
+        SUM(CAST(e.TOTAL_CLAIM_COST AS DECIMAL(18,2))) AS TotalClaimCost,
+        SUM(CAST(e.PAYER_COVERAGE AS DECIMAL(18,2))) AS TotalPayerCoverage,
+        (SUM(CAST(e.TOTAL_CLAIM_COST AS DECIMAL(18,2))) - SUM(CAST(e.PAYER_COVERAGE AS DECIMAL(18,2)))) AS UncoveredCost
+    FROM 
+        PROCEDURES p
+    JOIN 
+        ENCOUNTERS e ON p.ENCOUNTER = e.Id
+    GROUP BY 
+        p.CODE, p.DESCRIPTION, e.PAYER
+)
+SELECT 
+    ProcedureCode,
+    ProcedureDescription,
+    PAYER,
+    TotalBaseCost,
+    TotalClaimCost,
+    TotalPayerCoverage,
+    UncoveredCost
+FROM 
+    Procedure_Costs
+ORDER BY 
+    UncoveredCost DESC;
